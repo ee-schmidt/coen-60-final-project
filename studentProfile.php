404329a264748161938498100e401d53373c1813
merawson <meg.rawson1718@gmail.com>
2023-11-27 15:40:56 -0800

    v9
    studentProfile updated and profilestyle.css added

--- studentProfile.php
+++ studentProfile.php
@@ -1,1 +1,32 @@
-<h1>Student Profile</h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>Student User Profile</title>
+  <link rel="stylesheet" href="profilestyle.css">
+  <script src="java.js"></script>
+</head>
+
+<body> 
+  <div class = "BeigeBox"> 
+    <p id = "proftitle">STUDENT PROFILE</p>
+    <a href="../match/matchHome"> 
+      <button type = "button" id = "button">Match Now!</button>
+    </a>
+    <div class = "RedBox">
+    
+    <img src="squareIcon.png" alt="logo" width=300>
+    
+    <div class = "userinfo">
+    <div class = "red">
+    <h1 id = "name">First Last</h1> 
+    <h3> email </h3>
+    <h3>major + restricted/interdisciplinary</h3>
+    <p> more info here </p></div>
+  <a href="../match/matchHome"> 
+      <button type = "button">View Matches!</button>
+    </a>
+    </div>
+  </div>
+  </div>
+</div>
