d77c963db221b0b5f1961ab034c3d25d8c64a9e5
merawson <meg.rawson1718@gmail.com>
2023-11-30 00:57:46 -0800

    faculty profile and student profile

--- studentProfile.php
+++ studentProfile.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
+<link rel = "signup" href = "index.html">
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-  //$taken=[];
+  //$taken=["eeschmidt@scu.edu"];
   $student=$_POST["studentName"];
   //$faculty=$_POST["facultyEmail"];
   //$password1=$_POST["pwd1"];
   //$password2=$_POST["pwd2"];
 }
 /*if(in_array($studentEmail,$taken)){
-  echo "The account name '$email' is already taken.<br>";
+  echo "The email '$email' is already registered.<br>";
 }
 
 if(strlen($password)<8){
@@ -36,13 +37,32 @@ if(!in_array($email,$taken) && strlen($password)>=8 && is_numeric($password)){
 <body> 
   <div class = "BeigeBox"> 
     <p id = "proftitle">STUDENT PROFILE</p>
-    <a href="../match/matchHome"> 
+    <a href="studentMatch.html" target = "_blank"> 
       <button type = "button" id = "button">Match Now!</button>
     </a>
     <div class = "RedBox">
     
-    <img src="squareIcon.png" alt="logo" width=300>
-    
+    <?php 
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_FILES["profilePhoto"]) && $_FILES["profilePhoto"]["error"] === 0) {
+        $imagePath = basename($_FILES["profilePhoto"]["name"]);
+        $uploadFilePath = __DIR__ . '/' . $imagePath;
+
+        // Move the uploaded file to the current directory
+        if (move_uploaded_file($_FILES["profilePhoto"]["tmp_name"], $uploadFilePath)) {
+            // Display the uploaded image
+            //echo '<h2>Uploaded Profile Photo</h2>';
+            echo '<img src="' . $imagePath . '" alt="Uploaded Profile Photo" width = 300 height = 300>';
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    } else {
+        echo "Invalid file upload.";
+    }
+  } else {
+    echo "Invalid request.";
+  }
+?>
     <div class = "userinfo">
     <div class = "red">
     <h1 id = "name">
@@ -61,9 +81,28 @@ if(!in_array($email,$taken) && strlen($password)>=8 && is_numeric($password)){
        }
        ?>
     </h3>
-    <h3>major + restricted/interdisciplinary</h3>
-    <p> more info here </p></div>
-  <a href="../match/matchHome"> 
+    <h3>
+      <?php
+      if($_SERVER["REQUEST_METHOD"]=="POST"){
+          echo implode(' & ', $_POST['fmajor']);
+       }
+       ?>
+      major(s), 
+      <?php
+      if($_SERVER["REQUEST_METHOD"]=="POST"){
+        printf($_POST['interdis']); 
+      }
+      ?>
+      </h3>
+    <p>
+      <?php
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+          printf($_POST['extra']);
+        }
+        ?>
+
+       </p></div>
+  <a href="showMatches.html"> 
       <button type = "button">View Matches!</button>
     </a>
     </div>
